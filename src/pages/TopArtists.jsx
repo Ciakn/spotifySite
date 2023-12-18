185942ce72085c65cb925c895230674eba7933c5
ciavash <svsh.kian@gmail.com>
2023-12-18 06:54:00 -0800

    top Artists developed

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,7 @@
-const TopArtists = () => <div>TopArtists</div>;
+const TopArtists = () => (
+  <div className="text-3xl text-white font-bold flex justify-center items-center ">
+    Top Artists
+  </div>
+);
 
 export default TopArtists;
